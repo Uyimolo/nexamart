0f10726aeb96f4e7fb2c066f7264ce5a13d7a91b
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-04-25 12:42:22 +0100

    added loading components

--- src/components/lightbox/Lightbox.jsx
+++ src/components/lightbox/Lightbox.jsx
@@ -1,14 +1,34 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
 import style from './lightbox.module.css';
+import Icon from '../icon/Icon';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-const Lightbox = ({ imagesArray }) => {
-  console.log(imagesArray);
+const Lightbox = ({ imagesArray, error, isLoading }) => {
   const [currentImage, setCurrentImage] = useState(null);
 
   useEffect(() => {
     if (imagesArray) setCurrentImage(imagesArray[0]);
   }, [imagesArray]);
+
+  // if (error) return;
+
+  if (isLoading || error)
+    return (
+      <div className={`${style.lightbox_container} ${style.lightbox_loading}`}>
+        <div className={style.lightbox}>
+          <div className={style.current_image}>
+            <Icon icon={faSpinner} />
+          </div>
+
+          <div className={style.thumbnails}>
+            {[1, 2, 3, 4].map((image) => (
+              <div key={image} className={style.thumbnail}></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
 
   return (
     <div className={style.lightbox_container}>
